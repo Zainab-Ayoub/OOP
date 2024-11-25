141d16ce192b28e1bc55c9fd8a2e8537c10eeefb
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-11-25 23:03:44 +0500

    statement

---
+++ class/book.cpp
@@ -0,0 +1,7 @@
+// Write a class Book with three data members BookID, Pages and Price. 
+// It also contains the following member function:
+// The get() function is used to input values
+// The show() function is used to display values
+// The set() function is used to set the values of data members using parameters
+// getPrice() function is used to return the value of Price.
+
