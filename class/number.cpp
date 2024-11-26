208089c0e62c0dda98108e04713c79594decac9f
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-11-26 23:48:44 +0500

    final commit

--- class/number.cpp
+++ class/number.cpp
@@ -9,14 +9,25 @@ class Number{
             cout<<"Enter a number: ";
             cin>>n;
             if (n%2==0){
-                cout<<n<<" is even.";
+                cout<<n<<" is even."<<endl;
             }
             else{
-                cout<<n<<" is odd";
+                cout<<n<<" is odd"<<endl;
             }            
-        }    
+        }   
+        int factorial(int num){
+            n = num;
+            if (n==0){
+                return 1;
+            }
+            else{
+                return n*factorial(n-1);
+            }
+        } 
 };
 
 int main(){
-
+    Number x;
+    x.numType();
+    cout<<"Factorial of 5 is: "<<x.factorial(5);
 }
