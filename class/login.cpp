26208ec9710c0752f2d54ebbf76ca4c6d929ba1a
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-11-27 02:41:13 +0500

    final commit

--- class/login.cpp
+++ class/login.cpp
@@ -6,10 +6,10 @@ class abc{
         int password=12345, p1, p2;
         string username = "BSCS", u;
         char c;
-        int v, a[100], sum=0;
-        int max=0, min=1000;
+        int v, a[100], sum;
+        int max, min;
         int search;
-        int e=0, o=0, t=0;
+        int e, o, t;
 
     public:
         void login(){
@@ -29,10 +29,14 @@ class abc{
                             cout<<"Wrong Password, Try Again."<<endl;    
                     }
                     if(p1!=password){
-                        cout<<"Enter New Password: "<<endl;
-                        cin>>p2;
-                        password=p2;
-                        cout<<"Your password has been reset"<<endl;
+                        cout<<"Press 'C' to reset password.";
+                        cin>>c;
+                        if(c=='C'){
+                            cout<<"Enter New Password: "<<endl;
+                            cin>>p2;
+                            password=p2;
+                            cout<<"Your password has been reset"<<endl;
+                        }
                     }
                 }
                 else{
@@ -41,49 +45,51 @@ class abc{
             }
         }
         void menu(){
-            options:
-            cout<<"Press '1' to input values in an array & display the sum."<<endl;
-            cout<<"Press '2' to display maximum value of an array."<<endl;
-            cout<<"Press '3' to display minimum value of an array."<<endl;
-            cout<<"Press '4' to search the values in an array."<<endl;
-            cout<<"Press '5' to check EVEN/ODD values of an array."<<endl;
-            cout<<"Press '6' to check number of values in an array."<<endl;
-            cout<<"Press '0' to EXIT the program."<<endl;
-            cin>>c;
-            switch(c){
-                case '1':
-                    inputArray();
-                    goto options;
+            do{
+                cout<<"Press '1' to input values in an array & display the sum."<<endl;
+                cout<<"Press '2' to display maximum value of an array."<<endl;
+                cout<<"Press '3' to display minimum value of an array."<<endl;
+                cout<<"Press '4' to search the values in an array."<<endl;
+                cout<<"Press '5' to check EVEN/ODD values of an array."<<endl;
+                cout<<"Press '6' to check number of values in an array."<<endl;
+                cout<<"Press '0' to EXIT the program."<<endl;
+                cin>>c;
+                switch(c){
+                    case '1':
+                        inputArray();
+                        break;
 
-                case '2':
-                    findMax();
-                    goto options;
+                    case '2':
+                        findMax();
+                        break;
 
-                case '3':
-                    findMin();
-                    goto options;  
+                    case '3':
+                        findMin();
+                        break;
 
-                case '4':
-                    searchValue();
-                    goto options;
+                    case '4':
+                        searchValue();
+                        break;
 
-                case '5':
-                    evenOdd();
-                    goto options;
+                    case '5':
+                        evenOdd();
+                        break;
 
-                case '6':
-                    totalValue();
-                    goto options;
+                    case '6':
+                        totalValue();
+                        break;
 
-                case '0':
-                    cout<<"Application terminated successfully!";
-                    break;  
+                    case '0':
+                        cout<<"Application terminated successfully!";
+                        break;  
 
-                default:
-                    cout<<"Invalid Input";
-            }
+                    default:
+                        cout<<"Invalid Input";    
+                } 
+            } while (c!= '0');
         }
         void inputArray(){
+            sum=0;
             cout<<"How many values do you want to add? ";
             cin>>v;
             cout<<"Enter values: "<<endl;
@@ -97,6 +103,7 @@ class abc{
         }
         
         void findMax(){
+            max = a[0]; 
             for(int i=0; i<v; i++){
                 if(max<a[i])
                     max = a[i];
@@ -105,6 +112,7 @@ class abc{
         }
 
         void findMin(){
+            min = a[0]; 
             for(int i=0; i<v; i++){
                 if(min>a[i])
                     min = a[i];
@@ -122,6 +130,7 @@ class abc{
         }
 
         void evenOdd(){
+            e=0, o=0;
             for(int i=0; i<v; i++){
                 if(a[i]%2==0){
                     cout<<"Even: "<<a[i]<<endl;
@@ -137,6 +146,7 @@ class abc{
         }
 
         void totalValue(){
+            t=0;
             for(int i=0; i<v; i++){
                 t++;
             }
