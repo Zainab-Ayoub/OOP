18338a7a3876f38ae1c67ee3202caa48a716fdbe
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-11-26 23:45:09 +0500

    type function

--- class/number.cpp
+++ class/number.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 
 class Number{
-
+    private:
+        int n;
+    public:
+        void numType(){
+            cout<<"Enter a number: ";
+            cin>>n;
+            if (n%2==0){
+                cout<<n<<" is even.";
+            }
+            else{
+                cout<<n<<" is odd";
+            }            
+        }    
 };
 
 int main(){
-    
+
 }
