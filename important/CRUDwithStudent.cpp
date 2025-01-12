de3fb99663043f1b58f7e4528432b311ddf29791
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-01-12 15:52:53 +0500

    crud functions

--- important/CRUDwithStudent.cpp
+++ important/CRUDwithStudent.cpp
@@ -3,8 +3,49 @@ using namespace std;
 
 class Student{
     private:
-
+        int rollNo, marks, t, r;
+        string name;
+        int totalStudents=0;
     public:
+        Student(){
+            name="unknown";
+            rollNo=0;
+            marks=0;
+        }
+        void createInfo(){
+            cout<<"Enter name: ";
+            getline(cin,name);
+            cout<<"Enter roll number: ";
+            cin>>rollNo;
+            cout<<"Enter marks: ";
+            cin>>marks;
+            ++totalStudents;
+            t=totalStudents;
+        }
+        int readInfo(int r){
+            cout<<"Enter a roll number: ";
+            cin>>r;
+            if (r == rollNo){
+                cout<<"Name: "<<name<<endl;
+                cout<<"Roll Number: "<<rollNo<<endl;
+                cout<<"Marks: "<<marks<<endl;
+            } else{
+                cout<<"Wrong information, please try again!"<<endl;
+            }
+        }
+        void updateInfo(){
+            cout<<"Update your name and marks"<<endl;
+            cout<<"Enter name: ";
+            getline(cin,name);
+            cout<<"Enter marks: ";
+            cin>>marks;
+        }
+        void deleteInfo(){
+            name="";
+            rollNo=0;
+            marks=0;
+            cout<<"Your information has been deleted."<<endl;
+        }
 };
 
 int main(){
