61cadd2cc8acf0d5762ff9aabb6c47b41cc1e87e
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-02-02 23:27:30 +0500

    classes finalized

--- post-midTerm/inheritance/customerData.cpp
+++ post-midTerm/inheritance/customerData.cpp
@@ -6,13 +6,41 @@ class customerData{
         int customerNumber;
         string mailingAddress;
     public:
+        void inputCustomerData(){
+            cout<<"Enter Customer Number: ";
+            cin>>customerNumber;
+            cout<<"Enter Mailing Address: ";
+            cin>>mailingAddress;
+        }
+        void displayCustomerData(){
+            cout<<"Customer Number: "<<customerNumber<<endl;
+            cout<<"Mailing Address: "<<mailingAddress<<endl;
+        }
 
 };
 
 class preferredCustomer : public customerData{
     private:
         double purchaseAmount, discountLevel;
-    public:    
+    public:  
+        void inputCustomerData(){
+            customerData :: inputCustomerData();
+            cout<<"Enter Purchase Amount: $";
+            cin>>purchaseAmount;
+            if(purchaseAmount >= 2000){
+                discountLevel=10.0;
+            } else if(purchaseAmount >= 1500){
+                discountLevel=7.0; 
+            } else if(purchaseAmount >= 1000){
+                discountLevel=6.0; 
+            } else if(purchaseAmount >= 500){
+                discountLevel=5.0; 
+            }
+        }  
+        void displayCustomerData(){
+            customerData :: displayCustomerData();
+            cout<<"Discount Level: "<<discountLevel<<endl;
+        }
 };
 
 int main(){
