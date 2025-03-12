4f64dc8fa227a70eabd7eaa6fe793a3d469cda6a
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-03-12 07:06:31 +0500

    commit

--- pre-midTerm/important/bank.cpp
+++ pre-midTerm/important/bank.cpp
@@ -9,7 +9,6 @@ private:
     float balance_amount_in_the_account;
 
 public:
-    // Constructor to initialize account details
     bankAccount() {
         cout << "Name of the depositor: ";
         cin >> name_of_the_depositor;
@@ -21,13 +20,11 @@ public:
         cin >> balance_amount_in_the_account;
     }
 
-    // Function to deposit an amount
     void deposit(float d) {
         balance_amount_in_the_account += d;
         cout << "Deposited: " << d << endl;
     }
 
-    // Function to withdraw an amount
     void withdraw(float w) {
         if (w <= balance_amount_in_the_account) {
             balance_amount_in_the_account -= w;
@@ -37,7 +34,6 @@ public:
         }
     }
 
-    // Function to display account details
     void display() const {
         cout << "Name: " << name_of_the_depositor << endl;
         cout << "Account Number: " << account_number << endl;
@@ -45,7 +41,6 @@ public:
         cout << "Balance: " << balance_amount_in_the_account << endl;
     }
 
-    // Overloaded + operator to add balances of two accounts
     float operator+(bankAccount &other) {
         return balance_amount_in_the_account + other.balance_amount_in_the_account;
     }
@@ -57,18 +52,14 @@ int main() {
 
     cout << "\nEnter details for second account:" << endl;
     bankAccount account2;
-
-    // Display details of both accounts
     cout << "\nDetails of first account:" << endl;
     account1.display();
     cout << "\nDetails of second account:" << endl;
     account2.display();
 
-    // Deposit and withdraw operations for demonstration
     account1.deposit(1000);
     account2.withdraw(500);
 
-    // Add balances of both accounts
     float total_balance = account1 + account2;
 
     cout << "\nTotal balance of both accounts: " << total_balance << endl;
